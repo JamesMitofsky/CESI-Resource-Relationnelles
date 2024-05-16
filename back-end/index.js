628ed1a56e69590aff71956833b3504cac1c8864
Darko <todorovski.dar@gmail.com>
2024-05-16 16:11:37 +0200

    Add and Implement Cors to handle cross platform requests

--- back-end/index.js
+++ back-end/index.js
@@ -1,36 +1,45 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
 const app = express();
+
+// Cors Middleware
+app.use(cors());
 
 // BodyParser Middleware
 app.use(bodyParser.json());
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
 // Connect to MongoDB
 const connectionString = process.env.MONGODB_URI;
 
-mongoose.connect(connectionString)
-    .then(() => console.log('Successfully connected to MongoDB.'))
-    .catch(err => console.error('Connection error', err));
+mongoose
+  .connect(connectionString)
+  .then(() => console.log("Successfully connected to MongoDB."))
+  .catch((err) => console.error("Connection error", err));
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log("Successfully connected to the database.");
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Successfully connected to the database.");
 });
-  db.once('open', function() {
-    // we're connected!
-    console.log("We're connected to the database.");
-  });
+db.once("open", function () {
+  // we're connected!
+  console.log("We're connected to the database.");
+});
 
 // Use routes for your collections
-app.use('/api/users', require('./routes/users'));
-app.use('/api/resources', require('./routes/resources'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/resources", require("./routes/resources"));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
-
