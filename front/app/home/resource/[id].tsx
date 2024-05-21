33f73fd6407bf4a3797f974bfd2628ce2b9ea312
Darko <todorovski.dar@gmail.com>
2024-05-21 16:56:37 +0200

    Transform resource screen from static to dynamic, pass prop through link on home screen,  add a modal for modifying a resource.

--- front/app/home/resource/[id].tsx
+++ front/app/home/resource/[id].tsx
@@ -1,62 +1,47 @@
 import React, { useEffect, useState } from 'react';
+import { ScrollView, StyleSheet, Text } from 'react-native';
+import axios from 'axios';
 import {
-  AddIcon,
   ArrowLeftIcon,
-  Box,
+  Button,
+  ButtonIcon,
+  Modal,
+  ButtonText,
+  Card,
   Center,
   Checkbox,
-  Fab,
-  FabIcon,
-  FabLabel,
-  Heading,
-  HStack,
-  Input,
-  Select,
-  VStack,
-  ModalBackdrop,
-  ButtonText,
-  ModalContent,
-  ModalHeader,
-  ButtonIcon,
   CheckboxGroup,
-  InputField,
-  ModalBody,
-  Modal,
-  Button,
-  ModalFooter,
   CheckboxIcon,
   CheckboxIndicator,
   CheckboxLabel,
   CheckIcon,
-  SelectItem,
-  ChevronDownIcon,
-  Icon,
+  Heading,
+  HStack,
+  Input,
+  InputField,
+  ModalBackdrop,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  Select,
   SelectBackdrop,
   SelectContent,
   SelectDragIndicator,
   SelectDragIndicatorWrapper,
-  SelectIcon,
   SelectInput,
+  SelectItem,
   SelectPortal,
   SelectTrigger,
-  Image,
+  VStack,
 } from '@gluestack-ui/themed';
-import {
-  Pressable,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import axios from 'axios';
-import { Card } from '@gluestack-ui/themed';
-import { Resource } from '../../types/resource';
-import { BASE_URL } from '../../globals/port';
-import { useMediaQuery } from 'react-responsive';
-import { Link } from 'expo-router';
+import { Resource } from '../../../types/resource';
+import { BASE_URL } from '../../../globals/port';
+import { useLocalSearchParams } from 'expo-router';
 
 export default function App() {
-  const [resources, setResources] = useState<Resource[]>([]);
+  const { id } = useLocalSearchParams<{ id?: string }>();
+  const [resource, setResource] = useState<Resource | null>(null);
   const [showModal, setShowModal] = useState(false);
   const [title, setTitle] = useState('');
   const [type, setType] = useState('');
@@ -66,141 +51,58 @@ export default function App() {
   const [isFavorite, setIsFavorite] = useState(false);
 
   useEffect(() => {
-    const fetchResources = async () => {
+    const fetchResource = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}/api/resources`);
-        setResources(response.data);
-        console.log('Resources:', response.data);
+        const response = await axios.get(
+          `${BASE_URL}/api/resources/${id}`,
+        );
+        setResource(response.data);
       } catch (error) {
-        console.error('Error fetching resources:', error);
+        console.error(error);
       }
     };
 
-    fetchResources();
-  }, []);
+    fetchResource();
+  }, [id]);
 
+  const handleModifyResource = async () => {
+    console.log('Modify resource');
+  };
   const handleValueChange = (newValue: string) => {
-    // if (Array.isArray(newValue)) {
-    //   setCategories(newValue:);
-    // } else {
-    //   setCategories([newValue]);
-    // }
+    console.log('Modify value');
   };
 
-  const handleCreateResource = async () => {
-    const data = {
-      title: 'TEST Resource !!!',
-      type: 'Image',
-      categories: { categoryType: 'Image' },
-      uploader: { _id: '65cf5f1c21bf58f7b657b658' },
-      comments: [
-        {
-          content: 'salut ca va',
-          commenter: '65cf5f1c21bf58f7b657b658',
-        },
-      ],
-      isArchived: false,
-      isFavorite: false,
-    };
-
-    try {
-      const response = await axios.post(
-        `${BASE_URL}/api/resources`,
-        data,
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-    setShowModal(false);
-  };
-
-  const isWeb = useMediaQuery({ query: '(min-width: 768px)' });
-  let route: { pathname: string; params: { id: string } };
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <View
-        style={isWeb ? styles.webContainer : styles.mobileContainer}
-      >
-        {resources.map(resource => (
-          <Card key={resource._id} style={styles.card}>
-            <Text style={styles.title}>{resource.title}</Text>
-
-            {resource.categories.map(c => {
-              let imageUrl;
-              switch (c.categoryType) {
-                case 'Document':
-                  imageUrl =
-                    'https://fakeimg.pl/600x400/90c7bc/ffffff?text=Document';
-                  break;
-                case 'Other':
-                  imageUrl = 'https://fakeimg.pl/600x400?text=Other';
-                  break;
-                case 'Image':
-                  imageUrl =
-                    'https://fakeimg.pl/600x400/a492b0/ffffff?text=Image';
-                  break;
-                case 'Video':
-                  imageUrl =
-                    'https://fakeimg.pl/600x400/e08686/ffffff?text=Video';
-                  break;
-                case 'Audio':
-                  imageUrl =
-                    'https://fakeimg.pl/600x400/dbd993/ffffff?text=Audio';
-                  break;
-                default:
-                  imageUrl = 'https://fakeimg.pl/600x400?text=Other';
-              }
-              return (
-                <Image
-                  source={{ uri: imageUrl }}
-                  style={{
-                    width: 600,
-                    height: 250,
-                    borderRadius: 12,
-                    marginTop: 12,
-                  }}
-                />
-              );
-            })}
-            <Text style={styles.categories}>
-              Categories:{' '}
-              {resource.categories
-                .map(c => c.categoryType)
-                .join(', ')}
+    <ScrollView style={styles.container}>
+      <Card>
+        {resource ? (
+          <>
+            <Text>{resource.title}</Text>
+            <Text>Type: {resource.type}</Text>
+            <Text>Uploader: {resource.uploader}</Text>
+            <Text>
+              Is Archived: {resource.isArchived ? 'Yes' : 'No'}
             </Text>
-            <Text style={styles.categories}>
-              Description: {resource.type}
+            <Text>
+              Is Favorite: {resource.isFavorite ? 'Yes' : 'No'}
             </Text>
-
-            <Text style={styles.uploader}>
-              Uploader: {resource.uploader}
-            </Text>
-            <Text style={styles.status}>
-              Archived: {resource.isArchived ? 'Yes' : 'No'} |
-              Favorite: {resource.isFavorite ? 'Yes' : 'No'}
-            </Text>
-
-            <Link
-              href={{
-                pathname: `/home/resource/[id]`,
-                params: { id: resource._id },
-              }}
-            >
-              <Button
-                style={{
-                  marginTop: 12,
-                }}
-              >
-                <ButtonText>"Go to Resource"</ButtonText>
-              </Button>
-            </Link>
-          </Card>
-        ))}
-
-        <Button onPress={() => setShowModal(true)}>
-          <ButtonText>Create Resource</ButtonText>
-        </Button>
+            <Text>Categories:</Text>
+            {resource.categories.map((category, index) => (
+              <Text key={index}>{category.categoryType}</Text>
+            ))}
+            <Text>Comments:</Text>
+            {resource.comments.map(comment => (
+              <Text key={comment._id}>
+                {comment.content} - {comment.commenter}
+              </Text>
+            ))}
+            <Button onPress={() => setShowModal(true)}>
+              <ButtonText>Modify Resource</ButtonText>
+            </Button>
+          </>
+        ) : (
+          <Text>Loading...</Text>
+        )}
         <Center>
           <Modal
             isOpen={showModal}
@@ -297,7 +199,7 @@ export default function App() {
               </ModalBody>
               <ModalFooter borderTopWidth="$0">
                 <VStack space="lg" w="$full">
-                  <Button onPress={handleCreateResource}>
+                  <Button onPress={handleModifyResource}>
                     <ButtonText>Submit</ButtonText>
                   </Button>
                   <HStack space="xs" alignItems="center">
@@ -315,7 +217,7 @@ export default function App() {
             </ModalContent>
           </Modal>
         </Center>
-      </View>
+      </Card>
     </ScrollView>
   );
 }
@@ -324,16 +226,6 @@ const styles = StyleSheet.create({
   container: {
     padding: 16,
   },
-  webContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-between',
-  },
-  mobileContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
   card: {
     marginBottom: 16,
     padding: 16,
@@ -341,9 +233,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     shadowColor: '#000',
     shadowOpacity: 0.1,
-    shadowOffset: { width: 0, height: 2 },
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
     shadowRadius: 8,
-    flexBasis: '33%', // Adjust the percentage to fit your needs
   },
   title: {
     fontSize: 18,
