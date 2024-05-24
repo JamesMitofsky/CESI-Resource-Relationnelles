0dc01caa447a324dbbd69251efca8ed1ecc5ba55
Darko <todorovski.dar@gmail.com>
2024-05-24 14:43:14 +0200

    Add filter system and layout refactor

--- front/app/home/index.tsx
+++ front/app/home/index.tsx
@@ -64,19 +64,26 @@ export default function App() {
   const [uploader, setUploader] = useState('');
   const [isArchived, setIsArchived] = useState(false);
   const [isFavorite, setIsFavorite] = useState(false);
+  const [filter, setFilter] = useState('all');
+
+  const getAllResources = async () => {
+    try {
+      const response = await axios.get(`${BASE_URL}/api/resources`);
+      setResources(response.data);
+      console.log('Resources:', response.data);
+    } catch (error) {
+      console.error('Error fetching resources:', error);
+    }
+  };
+  const getFavouriteResources = async () => {
+    const response = await axios.get(
+      `${BASE_URL}/api/resources/favourites`,
+    );
+    setResources(response.data);
+  };
 
   useEffect(() => {
-    const fetchResources = async () => {
-      try {
-        const response = await axios.get(`${BASE_URL}/api/resources`);
-        setResources(response.data);
-        console.log('Resources:', response.data);
-      } catch (error) {
-        console.error('Error fetching resources:', error);
-      }
-    };
-
-    fetchResources();
+    getAllResources();
   }, []);
 
   const handleValueChange = (newValue: string) => {
@@ -141,11 +148,7 @@ export default function App() {
             position: 'relative',
           }}
         >
-          <Box
-            style={
-              isWeb ? styles.webContainer : styles.mobileContainer
-            }
-          >
+          <Box>
             <Box
               style={{
                 width: '100%',
@@ -169,78 +172,106 @@ export default function App() {
                 Ressources Relationnelles
               </Text>
             </Box>
-            {resources.map(resource => (
-              <Card key={resource._id} style={styles.card}>
-                <Text style={styles.title}>{resource.title}</Text>
+            <Box
+              style={{
+                justifyContent: 'center',
+                flexDirection: 'row',
+                gap: 16,
+                margin: 42,
+              }}
+            >
+              <Button onPress={getAllResources} style={styles.button}>
+                <ButtonText>All Resources</ButtonText>
+              </Button>
+              <Button
+                onPress={getFavouriteResources}
+                style={styles.button}
+              >
+                <ButtonText> Favourites</ButtonText>
+              </Button>
+            </Box>
+            <Box
+              style={
+                isWeb ? styles.webContainer : styles.mobileContainer
+              }
+            >
+              {resources.map(resource =>
+                filter === 'all' ||
+                filter ===
+                  'favourites' /*&& resource.isFavourite*/ ? (
+                  <Card key={resource._id} style={styles.card}>
+                    <Text style={styles.title}>{resource.title}</Text>
 
-                {resource.categories.map(c => {
-                  let imageUrl;
-                  switch (c.categoryType) {
-                    case 'Document':
-                      imageUrl =
-                        'https://fakeimg.pl/600x400/90c7bc/ffffff?text=Document';
-                      break;
-                    case 'Other':
-                      imageUrl =
-                        'https://fakeimg.pl/600x400?text=Other';
-                      break;
-                    case 'Image':
-                      imageUrl =
-                        'https://fakeimg.pl/600x400/a492b0/ffffff?text=Image';
-                      break;
-                    case 'Video':
-                      imageUrl =
-                        'https://fakeimg.pl/600x400/e08686/ffffff?text=Video';
-                      break;
-                    case 'Audio':
-                      imageUrl =
-                        'https://fakeimg.pl/600x400/dbd993/ffffff?text=Audio';
-                      break;
-                    default:
-                      imageUrl =
-                        'https://fakeimg.pl/600x400?text=Other';
-                  }
-                  return (
-                    <Image
-                      source={{ uri: imageUrl }}
-                      style={{
-                        width: 600,
-                        height: 250,
-                        borderRadius: 12,
-                        marginTop: 12,
+                    {resource.categories.map(c => {
+                      let imageUrl;
+                      switch (c.categoryType) {
+                        case 'Document':
+                          imageUrl =
+                            'https://fakeimg.pl/600x400/90c7bc/ffffff?text=Document';
+                          break;
+                        case 'Other':
+                          imageUrl =
+                            'https://fakeimg.pl/600x400?text=Other';
+                          break;
+                        case 'Image':
+                          imageUrl =
+                            'https://fakeimg.pl/600x400/a492b0/ffffff?text=Image';
+                          break;
+                        case 'Video':
+                          imageUrl =
+                            'https://fakeimg.pl/600x400/e08686/ffffff?text=Video';
+                          break;
+                        case 'Audio':
+                          imageUrl =
+                            'https://fakeimg.pl/600x400/dbd993/ffffff?text=Audio';
+                          break;
+                        default:
+                          imageUrl =
+                            'https://fakeimg.pl/600x400?text=Other';
+                      }
+                      return (
+                        <Image
+                          source={{ uri: imageUrl }}
+                          style={{
+                            width: 600,
+                            height: 250,
+                            borderRadius: 12,
+                            marginTop: 12,
+                          }}
+                        />
+                      );
+                    })}
+                    <Text style={styles.categories}>
+                      Categories:{' '}
+                      {resource.categories
+                        .map(c => c.categoryType)
+                        .join(', ')}
+                    </Text>
+
+                    <Text style={styles.uploader}>
+                      Uploader: {resource.uploader}
+                    </Text>
+                    <Text style={styles.status}>
+                      Archived: {resource.isArchived ? 'Yes' : 'No'} |
+                      Featured ❤️: {resource.isFavorite ? '🤩' : '😟'}
+                    </Text>
+                    <Text style={styles.categories}>
+                      Comments: {resource.comments.length}
+                    </Text>
+
+                    <Link
+                      href={{
+                        pathname: `/home/resource/[id]`,
+                        params: { id: resource._id },
                       }}
-                    />
-                  );
-                })}
-                <Text style={styles.categories}>
-                  Categories:{' '}
-                  {resource.categories
-                    .map(c => c.categoryType)
-                    .join(', ')}
-                </Text>
-
-                <Text style={styles.uploader}>
-                  Uploader: {resource.uploader}
-                </Text>
-                <Text style={styles.status}>
-                  Archived: {resource.isArchived ? 'Yes' : 'No'} |
-                  Featured ❤️: {resource.isFavorite ? '🤩' : '😟'}
-                </Text>
-                <Text style={styles.categories}>
-                  Comments: {resource.comments.length}
-                </Text>
-
-                <Link
-                  href={{
-                    pathname: `/home/resource/[id]`,
-                    params: { id: resource._id },
-                  }}
-                  style={styles.linkButton}
-                >
-                  <Text>Go to Resource</Text>
-                </Link>
-              </Card>
-            ))}
+                      style={styles.linkButton}
+                    >
+                      <Text>Go to Resource</Text>
+                    </Link>
+                  </Card>
+                ) : null,
+              )}
+            </Box>
           </Box>
 
           <Button
@@ -385,14 +416,13 @@ const styles = StyleSheet.create({
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    justifyContent: 'space-between',
+    gap: 16,
   },
   mobileContainer: {
     display: 'flex',
     flexDirection: 'column',
   },
   card: {
-    marginBottom: 16,
     padding: 16,
     borderRadius: 8,
     backgroundColor: '#fff',
@@ -400,7 +430,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
-    flexBasis: '33%', // Adjust the percentage to fit your needs
+    flexBasis: '32%', // Adjust the percentage to fit your needs
   },
   title: {
     fontSize: 18,
@@ -426,15 +456,19 @@ const styles = StyleSheet.create({
     color: '#666',
   },
   linkButton: {
-    backgroundColor: '#007BFF',
+    backgroundColor: '#020092',
     color: '#FFFFFF',
     paddingVertical: 10,
     paddingHorizontal: 12,
-    borderRadius: 4,
-    marginTop: 8,
+    borderRadius: 12,
     textAlign: 'center',
     fontSize: 16,
+    marginTop: 8,
     fontWeight: '600',
     textDecorationLine: 'none',
+  },
+  button: {
+    borderRadius: 12,
+    backgroundColor: '#020092',
   },
 });
