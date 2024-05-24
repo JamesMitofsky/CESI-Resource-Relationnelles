9aea88397526e012aeda679fbf50a8e500919898
Darko <todorovski.dar@gmail.com>
2024-05-24 14:41:00 +0200

    Add a new route to get featured resources

--- back-end/routes/resources.js
+++ back-end/routes/resources.js
@@ -1,38 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Load Resource model
-const Resource = require('../models/Resource');
+const Resource = require("../models/Resource");
 
 // @route   POST api/resources
 // @desc    Create a resource
-router.post('/', async (req, res) => {
-    const { title, type, categories, uploader, comments, isArchived, isFavorite } = req.body;
-  
-    const newResource = new Resource({
-      title,
-      type,
-      categories,
-      uploader,
-      comments,
-      isArchived,
-      isFavorite
-    });
-  
-    try {
-      const resource = await newResource.save();
-      res.status(201).json(resource);
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
+router.post("/", async (req, res) => {
+  const {
+    title,
+    type,
+    categories,
+    uploader,
+    comments,
+    isArchived,
+    isFavorite,
+  } = req.body;
+
+  const newResource = new Resource({
+    title,
+    type,
+    categories,
+    uploader,
+    comments,
+    isArchived,
+    isFavorite,
   });
+
+  try {
+    const resource = await newResource.save();
+    res.status(201).json(resource);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Route to get all favourite resources
+router.get("/favourites", async (req, res) => {
+  try {
+    const favourites = await Resource.find({ isFavorite: true });
+    res.json(favourites);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 // @route   GET api/resources
 // @desc    Get all resources
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const resources = await Resource.find();
-     res.json(resources);
+    res.json(resources);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -40,51 +59,59 @@ router.get('/', async (req, res) => {
 
 // @route   GET api/resources/:id
 // @desc    Get a resource by id
-router.get('/:id', getResource, (req, res) => {
+router.get("/:id", getResource, (req, res) => {
   res.json(res.resource);
 });
 
 // @route   PUT api/resources/:id
 // @desc    Update a resource
-router.put('/:id', getResource, async (req, res) => {
-    const { title, type, categories, uploader, comments, isArchived, isFavorite } = req.body;
-  
-    if (title != null) {
-      res.resource.title = title;
-    }
-    if (type != null) {
-      res.resource.type = type;
-    }
-    if (categories != null) {
-      res.resource.categories = categories;
-    }
-    if (uploader != null) {
-      res.resource.uploader = uploader;
-    }
-    if (comments != null) {
-      res.resource.comments = comments;
-    }
-    if (isArchived != null) {
-      res.resource.isArchived = isArchived;
-    }
-    if (isFavorite != null) {
-      res.resource.isFavorite = isFavorite;
-    }
-  
-    try {
-      const updatedResource = await res.resource.save();
-      res.json(updatedResource);
-    } catch (err) {
-      res.status(400).json({ error: err.message });
-    }
-  });
+router.put("/:id", getResource, async (req, res) => {
+  const {
+    title,
+    type,
+    categories,
+    uploader,
+    comments,
+    isArchived,
+    isFavorite,
+  } = req.body;
+
+  if (title != null) {
+    res.resource.title = title;
+  }
+  if (type != null) {
+    res.resource.type = type;
+  }
+  if (categories != null) {
+    res.resource.categories = categories;
+  }
+  if (uploader != null) {
+    res.resource.uploader = uploader;
+  }
+  if (comments != null) {
+    res.resource.comments = comments;
+  }
+  if (isArchived != null) {
+    res.resource.isArchived = isArchived;
+  }
+  if (isFavorite != null) {
+    res.resource.isFavorite = isFavorite;
+  }
+
+  try {
+    const updatedResource = await res.resource.save();
+    res.json(updatedResource);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
 
 // @route   DELETE api/resources/:id
 // @desc    Delete a resource
-router.delete('/:id', getResource, async (req, res) => {
+router.delete("/:id", getResource, async (req, res) => {
   try {
     await res.resource.deleteOne();
-    res.json({ message: 'Deleted Resource' });
+    res.json({ message: "Deleted Resource" });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
@@ -96,7 +123,7 @@ async function getResource(req, res, next) {
   try {
     resource = await Resource.findById(req.params.id);
     if (resource == null) {
-      return res.status(404).json({ message: 'Cannot find resource' });
+      return res.status(404).json({ message: "Cannot find resource" });
     }
   } catch (err) {
     return res.status(500).json({ error: err.message });
