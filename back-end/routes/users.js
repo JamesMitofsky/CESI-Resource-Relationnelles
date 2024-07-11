a94d2da701f9bca61192c7a5947484e0807a84a8
Darko <todorovski.dar@gmail.com>
2024-07-11 15:31:53 +0200

    docker files, dockerignore, docker compose file , backend fix bcrypt to bcryptjs

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const saltRounds = 10;
 
 const secretOrKey = process.env.SECRET_OR_KEY;
