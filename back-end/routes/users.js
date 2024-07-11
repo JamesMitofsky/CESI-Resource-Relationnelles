bef2dc1820cb88b7216e58224f1c49c39760794b
Darko <todorovski.dar@gmail.com>
2024-07-11 16:54:56 +0200

    test pipeline

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -17,7 +17,7 @@ router.post("/login", async (req, res) => {
   if (!user) {
     return res.status(404).json({ email: "User not found" });
   }
-
+  //test
   // Check password
   bcrypt.compare(password, user.password).then((isMatch) => {
     if (isMatch) {
